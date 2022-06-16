0c7c34cc8a508211ef995ab371b5eb49c17cf423
Alfredo Scalera <alfredo.scalera.2019@uni.strath.ac.uk>
2022-06-16 18:40:30 +0100

    added instructions to networkDesigner.py

--- networdDesigner.py
+++ networdDesigner.py
@@ -7,3 +7,34 @@
 
 """
 
+class networkDesigner:
+    """
+    
+    main class that houses algorithm
+    manages inter-node methods
+    
+    """
+    
+    def __init__(self):
+       pass 
+        
+class Node:
+    """
+    
+    needs:
+        
+    position
+    name
+    parent node indicator (can be string of parent node name)
+    current matrix/list from hour 0 to 8760
+    voltage //
+    current status (calculated or not)
+    voltage //
+    
+    connection matrix? (list of nodes connected to, )
+    
+    """
+    
+    def __init__(self):
+        pass
+    
