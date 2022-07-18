e23962d226d2a8101bd3249b93b36888c3dc8531
Alfredo Scalera <alfredo.scalera.2019@uni.strath.ac.uk>
2022-07-18 16:50:44 +0100

    added initial imports in customer_clustering

--- customer_clustering.py
+++ customer_clustering.py
@@ -1,7 +1,36 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Jul 18 16:19:29 2022
 
-@author: fredo
+    Customer Clustering for "Energy 4 Development" VIP
+    
+    Code by Alfredo Scalera (alfredo.scalera.2019@uni.strath.ac.uk)
+
 """
 
+import numpy as np
+import pandas as pd
+import k_means_constrained as kms
+
+def import_nodes_from_csv(scale_factor=1):
+    
+    scale = scale_factor
+    
+    # read CSV file
+    df = pd.read_csv("nodes.csv")
+    df = df.set_index("ID")
+    
+    nodes = []
+    
+    # create source and node objects from entries in CSV
+    source = True
+    for node_id,data in df.iteritems():
+        # first entry is source
+        if source:
+            source_location = [scale * int(data[0]), scale * int(data[1])]
+            nodes.append(Source(source_location))
+            source = False
+        # rest are nodes
+        else:
+            location = [scale * int(data[0]), scale * int(data[1])]
+            power_demand = data[2:].tolist()
+            nodes.append(Node(location, node_id, power_demand))
