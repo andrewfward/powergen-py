a3bc5bcefb79654ce2e5c19ddfbd8dec8889cbce
Alfredo Scalera <alfredo.scalera.2019@uni.strath.ac.uk>
2022-07-18 16:21:23 +0100

    added customer clustering susbystem and utf-8 encoding banner in network designer

---
+++ customer_clustering.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 18 16:19:29 2022
+
+@author: fredo
+"""
+
--- networkDesigner2.py
+++ networkDesigner2.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
 
     Network Designer for "Energy 4 Development" VIP
