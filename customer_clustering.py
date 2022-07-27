09a0b7f9c59a99f5c590cc64587514492cc9e43e
Alfredo Scalera <alfredo.scalera.2019@uni.strath.ac.uk>
2022-07-27 16:25:40 +0200

    added customer clustering file

---
+++ customer_clustering.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+
+    Customer Clustering
+    
+    "Energy For Development" VIP (University of Strathclyde)
+    
+    Code by Alfredo Scalera (alfredo.scalera.2019@uni.strath.ac.uk)
+    
+"""
+
