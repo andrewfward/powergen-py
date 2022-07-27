0beb1e40efbeb6151f8bd4a4ee840cd89f64b6d6
Alfredo Scalera <alfredo.scalera.2019@uni.strath.ac.uk>
2022-07-27 16:54:14 +0200

    defined init method for CustomerClustering in customer_clustering

--- customer_clustering.py
+++ customer_clustering.py
@@ -9,3 +9,34 @@
     
 """
 
+import numpy as np
+import pandas as pd
+from sklearn.cluster import KMeans
+
+
+class CustomerClustering:
+    
+    def __init__(max_connections, network_voltage, pole_cost
+                 resistance_per_km, current_rating, cost_per_km,
+                 max_voltage_drop=None, max_distance = None):
+        
+        # network parameters
+        self.max_connections = max_connections
+        self.network_voltage = network_voltage
+        
+        # pole parameters
+        self.pole_cost = pole_cost
+        
+        # cable parameters
+        self.res_m = resistance_per_km / 1000
+        self.current_rating = current_rating
+        self.cost_m = cost_per_km / 1000
+        
+    
+    @classmethod
+    def import_from_csv(cls, filename, max_connections,
+                        network_voltage, pole_cost, resistance_per_km,
+                        current_rating, cost_per_km,
+                        max_voltage_drop=None, max_distance = None)
+    
+    
