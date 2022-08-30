7370e64a5559a05406f57445836fd02ed85bc894
Alfredo Scalera <alfredo.scalera.2019@uni.strath.ac.uk>
2022-08-30 20:05:42 +0200

    created economic evaluation subsystem python file

---
+++ economic_eval.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+
+    Generation Sizer for "Energy 4 Development" VIP (U. of Strathclyde)
+    
+    Code by Alfredo Scalera (alfredo.scalera.2019@uni.strath.ac.uk)
+    
+    Based on MATLAB code by Steven Nolan ( ).
+
+"""
