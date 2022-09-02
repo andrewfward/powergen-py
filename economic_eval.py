8f9b9fe16812eba7869ba12e3ad8a35a52af82fd
Alfredo Scalera <alfredo.scalera.2019@uni.strath.ac.uk>
2022-09-02 18:25:14 +0200

    updated comment in economical_eval.py

--- economic_eval.py
+++ economic_eval.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 
-    Generation Sizer for "Energy 4 Development" VIP (U. of Strathclyde)
+    Economic Evaluation for "Energy 4 Development" VIP (U. of Strathclyde)
     
     Code by Alfredo Scalera (alfredo.scalera.2019@uni.strath.ac.uk)
     
