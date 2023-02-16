563177f03e21d2a66e2f5fb5fc12db7f1a205b7d
Finlay Muir <116896403+finmuir@users.noreply.github.com>
2023-02-16 16:43:16 +0000

    Create app.js

---
+++ MappingTest2/static/js/app.js
@@ -0,0 +1,37 @@
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 51.5074, lng: 0.1278 },
+    zoom: 8,
+  });
+
+  let markers = [];
+  let path = new google.maps.Polyline({
+    path: markers,
+    geodesic: true,
+    strokeColor: "#FF0000",
+    strokeOpacity: 1.0,
+    strokeWeight: 2,
+  });
+  path.setMap(map);
+
+  google.maps.event.addListener(map, "click", (event) => {
+    addMarker(event.latLng);
+  });
+
+  function addMarker(location) {
+    let marker = new google.maps.Marker({
+      position: location,
+      map: map,
+      draggable: true,
+    });
+    markers.push(marker.getPosition());
+    path.setPath(markers);
+
+    google.maps.event.addListener(marker, "dragend", () => {
+      markers[markers.indexOf(marker.getPosition())] = marker.getPosition();
+      path.setPath(markers);
+    });
+  }
+}
